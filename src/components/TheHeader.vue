dde74b92ef84b35c1b04504a8afe57f703dda810
Shakar Kosar <shakarkosar01@gmail.com>
2024-07-18 19:39:08 +0300

    feat: fixing layout

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,21 @@
 <template>
-	<ul>
+	<ul class="header">
 		<li @click="$router.push('/')">Upload</li>
 		<li @click="$router.push('query')">Query</li>
 	</ul>
 </template>
+
+<style lang="scss" scoped>
+.header {
+	height: 30px;
+	width: 100%;
+	display: flex;
+	justify-items: center;
+	align-items: center;
+	list-style: none;
+
+	li {
+		margin: 0 4px;
+	}
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,30 @@
 <template>
 	<div>
 		<the-header />
-		<router-view />
+		<div class="container">
+			<div class="page">
+				<router-view />
+			</div>
+		</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
 import TheHeader from '@/components/TheHeader.vue';
 </script>
+
+<style lang="scss" scoped>
+.container {
+	display: grid;
+	background-color: #d3d3d3;
+	width: 100%;
+	height: 100vh;
+}
+
+.page {
+	margin: 20px;
+	padding: 20px;
+	background-color: white;
+	height: 80%;
+}
+</style>
